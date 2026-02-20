1b97b22780a06cedfa4d1c2ea95f5c58d4dc40c3
mohanadAdam <mohaned.adam2016@gmail.com>
2026-02-20 07:32:53 +0200

    fix coderabbit saggestion

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
         <header>
             <div className="main-cotainer inner">
              <Link href = "/">
-                <Image src = "logo.svg" alt = "Coinpulse logo" width={132} height={40} />
+                <Image src = "/logo.svg" alt = "Coinpulse logo" width={132} height={40} />
              </Link>
              <nav>
                  <Link href ='/' className={cn('nav-link',{'is-active':Pathname ==='/', 'is-home':true})}>Home</Link>
