4695b37226ce440165606582da61d730a3da6350
mohanadAdam <mohaned.adam2016@gmail.com>
2026-02-20 07:04:38 +0200

    implement navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
 const Page = () => {
-    return <p className="text-3xl text-indigo-500"> Coinpulse</p>
-
+    return (
+        <div></div>
+    )
 }
 export default Page
